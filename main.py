99ef15d9c00c3258738d47d6c8a6e27930269c99
ABC10946 <abctech10946@gmail.com>
2025-03-05 00:55:25 +0900

    操作配列を人間に分かりやすくした

--- main.py
+++ main.py
@@ -71,132 +71,27 @@ def clearDragons(game):
 
 
 def main():
-    backhiRaw = [["B6", "DD", "DD", "FF", "B2"], # left 0が手前 4が奥側
-              ["B8", "DC", "B1", "C6", "K2"],
-              ["DC", "B5", "C9", "C8", "K1"],
-              ["DC", "C3", "K3", "B3", "DH"],
-              ["C2", "DC", "DD", "K7", "K9"],
-              ["K8", "B9", "DD", "DH", "K6"],
-              ["DH", "C5", "DH", "B7", "C4"],
-              ["B4", "C7", "C1", "K5", "K4"]]   # right
+    backhiRaw = [["FF", "C7", "DC", "DH", "C6"], # left 0が手前 4が奥側
+              ["C1", "DC", "B8", "B9", "B1"],
+              ["K1", "DH", "B3", "K3", "K6"],
+              ["DH", "DH", "B4", "K4", "K5"],
+              ["DD", "C4", "C8", "B7", "C3"],
+              ["B6", "K3", "K9", "DC", "DC"],
+              ["B2", "C9", "C5", "DD", "K2"],
+              ["B5", "DD", "K7", "DD", "K2"]]   # right
 
     backHiConverted = [[convertToHiClass(x) for x in a] for a in backhiRaw]
 
     game = Solitaire(backHiConverted)
     # control(game,     pickHiX=None, pickLeft=None, targetX=None, targetCenter=None, targetLeft=None, targetRight=None)
     # game.printField()
-    # control(game, 7, None, None, None, 2, None)
-    # game.printField()
-    # control(game, 7, None, 5, None, None, None)
-    # game.printField()
-    # control(game, 7, None, None, None, None, 0)
-    # game.printField()
-    # control(game, 4, None, None, None, None, 0)
-    # game.printField()
-    # control(game, 0, None, 5, None, None, None)
-    # game.printField()
-    # control(game, 7, None, 5, None, None, None)
-    # game.printField()
-    # control(game, None, 2, 5, None, None, None)
-    # game.printField()
-    # control(game, 7, None, None, None, 2, None)
-    # game.printField()
-    # control(game, 5, None, 7, None, None, None)
-    # game.printField()
-    # control(game, 4, None, None, None, 1, None)
-    # game.printField()
-    # control(game, 1, None, None, None, 0, None)
-    # game.printField()
-
-    # game.clearDragon()
-    # game.printField()
-
-    # control(game, 1, None, None, None, None, 1)
-    # game.printField()
-
-    # control(game, 2, None, 1, None, None, None)
-    # game.printField()
-    
-    # control(game, None, 2, 1, None, None, None)
-    # game.printField()
-
-    # control(game, None, 0, 2, None, None, None)
-    # game.printField()
-
-    # control(game, 4, None, None, None, 0, None)
-    # game.printField()
-
-    # control(game, 0, None, None, None, 2, None)
-    # game.printField()
-
-    # game.clearDragon()
-    # game.printField()
-
-    # control(game, 0, None, None, True, None, None)
-    # game.printField()
-
-    # control(game, 0, None, None, None, None, 1)
-    # game.printField()
-
-    # control(game, 1, None, 4, None, None, None)
-    # game.printField()
-
-    # control(game, 4, None, 2, None, None, None)
-    # game.printField()
-
-    # control(game, 2, None, 0, None, None, None)
-    # game.printField()
-
-    # control(game, 2, None, 4, None, None, None)
-    # game.printField()
-
-    # control(game, 2, None, None, None, None, 2)
-    # game.printField()
-
-    # control(game, 1, None, None, None, None, 2)
-    # game.printField()
-
-    # control(game, 3, None, None, None, None, 0)
-    # game.printField()
-
-    # control(game, 3, None, None, None, None, 2)
-    # game.printField()
-
-    # control(game, 3, None, None, None, None, 1)
-    # game.printField()
-
-    # control(game, 0, None, None, None, None, 2)
-    # game.printField()
-
-    # control(game, 7, None, None, None, None, 1)
-    # game.printField()
-
-    # control(game, 0, None, 4, None, None, None, 3)
-    # game.printField()
-
-    # control(game, 7, None, 0, None, None, None, 3)
-    # game.printField()
-
-    # control(game, 6, None, None, None, 2, None)
-    # game.printField()
-
-    # control(game, 6, None, 2, None, None, None)
-    # game.printField()
-
-    # game.clearDragon()
-    # game.printField()
-
-    # control(game, 5, None, 6, None, None, None)
-    # game.printField()
-
-    # control(game, 6, None, 7, None, None, None)
-    # game.printField()
-    
+    control(game, 0, None, None, True, None, None)
+    game.printField()
+    control(game, 1, None, None, None, None, 0)
+    game.printField()
+    control(game, 2, None, None, None, None, 1)
+    game.printField()
     print("=======solver======")
-
-
-    # while not solver(game):
-    #     game.printField()
 
     # Node(parent, game, controlArr, children=[], depth=0, dead=False):
     node = Node(None, game, [None, None, None, None, None, None, None, None], [], depth=0, dead=False)
@@ -216,7 +111,34 @@ def main():
     node = reversed(nodeArr)
 
     for n in node:
-        print(n.controlArr)
+        # print(n.controlArr)
+        # n.controlArrを人間に分かりやすい形に変換
+        controlArr = n.controlArr
+        if controlArr[0] is not None:
+            if controlArr[2] is not None:
+                if controlArr[6] is not None:
+                    print("pick from hiX: " + str(controlArr[0]) + " to targetX: " + str(controlArr[2]) + " length: " + str(controlArr[6]))
+                else:
+                    print("pick from backhiX: " + str(controlArr[0]) + " to targetX: " + str(controlArr[2]))
+
+            elif controlArr[3] is not None:
+                print("pick from backhiX: " + str(controlArr[0]) + " to targetCenter")
+            elif controlArr[4] is not None:
+                print("pick from backhiX: " + str(controlArr[0]) + " to targetLeft:" + str(controlArr[4]))
+            elif controlArr[5] is not None:
+                print("pick from backhiX: " + str(controlArr[0]) + " to targetRight:" + str(controlArr[5]))
+        elif controlArr[1] is not None:
+            if controlArr[2] is not None:
+                print("pick from left: " + str(controlArr[1]) + " to targetX: " + str(controlArr[2]))
+            elif controlArr[3] is not None:
+                print("pick from left: " + str(controlArr[1]) + " to targetCenter")
+            elif controlArr[4] is not None:
+                print("pick from left: " + str(controlArr[1]) + " to targetLeft:" + str(controlArr[4]))
+            elif controlArr[5] is not None:
+                print("pick from left: " + str(controlArr[1]) + " to targetRight:" + str(controlArr[5]))
+        elif controlArr[7] is not None:
+            print("clearDragon")
+
         n.game.printField()
 
 
